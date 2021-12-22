1c29d2aacc2d7d04b7338955d41759512da0fd45
rumeone <yuaroslav_29@mail.ru>
2021-12-22 22:05:06 +0700

    Add alpha version modal window

--- assets/modelWindow.js
+++ assets/modelWindow.js
@@ -1,7 +1,19 @@
-const onlineList = document.querySelector(".online_list");
-const modalWindow = document.querySelector(".modalWindowList")
+/*const onlineList = document.querySelector(".online_list");
+const modalWindow = document.querySelector(".modalWindowList");*/
 
 /*onlineList.addEventListener("click", () => {
 
 })*/
 
+const openModalWindow = () => {
+    const onlineList = document.querySelector(".online_list");
+    const modalWindow = document.querySelector(".modalWindowList");
+    if (!onlineList || !modalWindow)
+        return;
+    onlineList.addEventListener("click", (e) => {
+        e.preventDefault()
+        modalWindow.classList.add("modal_active")
+    })
+};
+
+openModalWindow();
